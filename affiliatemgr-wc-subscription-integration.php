d3744fd4d336603b5f57da4781be93fa1704df52
affmngr <affmngr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 03:23:50 +0000

    .first commit
    git-svn-id: https://plugins.svn.wordpress.org/affiliates-manager-woocommerce-subscription-integration/trunk@1098870 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ affiliatemgr-wc-subscription-integration.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+  Plugin Name: Affiliates Manager WooCommerce Subscription Integration
+  Plugin URI: https://wpaffiliatemanager.com/affiliates-manager-woocommerce-subscription-integration/
+  Description: Process an affiliate commission via Affiliates Manager after a WooCommerce subscription payment.
+  Version: 1.0.1
+  Author: wp.insider, affmngr
+  Author URI: https://wpaffiliatemanager.com
+ */
+
+add_action('processed_subscription_payments_for_order', 'wp_aff_handle_woocommerce_subscription_payment');  //Triggered when a subscription payment is made
+
+function wpam_handle_woocommerce_subscription_payment($order) {
+    if (!is_object($order)) {
+        $order = new WC_Order($order);
+    }
+
+    $order_id = $order->id;
+    $total = $order->order_total;
+    $shipping = $order->get_total_shipping();
+    $tax = $order->get_total_tax();
+    WPAM_Logger::log_debug('WooCommerce Subscription Integration - Total amount: ' . $total . ', Total shipping: ' . $shipping . ', Total tax: ' . $tax);
+    $purchaseAmount = $total - $shipping - $tax;
+    $wpam_refkey = get_post_meta($order_id, '_wpam_refkey', true);
+    if (empty($wpam_refkey)) {
+        WPAM_Logger::log_debug("WooCommerce Subscription Integration - could not get wpam_refkey from cookie. This is not an affiliate sale");
+        return;
+    }
+    $order_status = $order->status;
+    WPAM_Logger::log_debug("WooCommerce Subscription Integration - Order status: " . $order_status);
+    if (strtolower($order_status) != "completed" && strtolower($order_status) != "processing") {
+        WPAM_Logger::log_debug("WooCommerce Subscription Integration - Order status for this transaction is not in a 'completed' or 'processing' state. Commission will not be awarded at this stage.");
+        WPAM_Logger::log_debug("WooCommerce Subscription Integration - Commission for this transaciton will be awarded when you set the order status to completed or processing.");
+        return;
+    }
+    $requestTracker = new WPAM_Tracking_RequestTracker();
+    WPAM_Logger::log_debug('WooCommerce Subscription Integration - awarding commission for order ID: ' . $order_id . '. Purchase amount: ' . $purchaseAmount);
+    $requestTracker->handleCheckoutWithRefKey($order_id, $purchaseAmount, $wpam_refkey);
+}
+
+
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Affiliates Manager WooCommerce Subscription Integration ===
+Contributors: wp.insider, affmngr
+Donate link: https://wpaffiliatemanager.com/
+Tags: affiliate, affiliates, affiliates manager, integration, woocommerce, checkout, subscription
+Requires at least: 3.8
+Tested up to: 4.1
+Stable tag: 1.0.1
+License: GPLv2 or later
+
+Process an affiliate commission via Affiliates Manager plugin after a WooCommerce subscription payment
+
+== Description ==
+
+This addon allows you to integrate WooCommerce subscription extension with the Affiliates Manager plugin.
+
+When a recurring payment is charged, this addon will check to see if the original sale was referred to your site by an affiliate. It will then give appropriate commission to the affiliate.
+
+This addon requires the [Affiliates Manager Plugin](https://wordpress.org/plugins/affiliates-manager/).
+
+Read the usage documentation [here](https://wpaffiliatemanager.com/affiliates-manager-woocommerce-subscription-integration/).
+
+== Installation ==
+
+1. Upload `affiliates-manager-woocommerce-subscription-integration` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+None
+
+== Screenshots ==
+Check the following page for screenshots
+https://wpaffiliatemanager.com/
+
+== Changelog ==
+
+= 1.0.1 =
+* First commit to wordpress.org
+
+== Upgrade Notice ==
+None
